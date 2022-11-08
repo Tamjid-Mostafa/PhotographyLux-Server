0fff3c81e023110837b245fb8005df0773335307
Tamjid-Mostafa <tamjid430@gmail.com>
2022-11-08 23:55:48 +0600

    added get method and limit services

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express();
 require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j8jry5z.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+const run = async () => {
+  try {
+    const servicesCollection = await client
+      .db("photographylux")
+      .collection("services");
 
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+};
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+app.listen(port, () => {
+  console.log(`Project is running : ${port}`);
+});
